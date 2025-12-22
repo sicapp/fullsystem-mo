79384f3f7787c553ab4137127bc808f21d386d14
sergioicarvalho <sergio.carvalho@melibox.com.br>
2025-12-22 13:14:43 -0300

    Busca automática de anuncios no Mercado Livre e Produtos no Bling

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,12 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'jobs' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/jobs.log'),
+            'level' => 'info',
+        ],
+
     ],
 
 ];
